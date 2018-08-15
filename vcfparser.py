4ec62dc47f8c211e243ddeb9b0de83b2995914ba
Richard Crooks <email@rocrooks.co.uk>
2018-08-15 15:24:15 +0100

    Retrieve HGVS format from variant description. Currently does not handle insertions or deletions correctly

--- vcfparser.py
+++ vcfparser.py
@@ -10,6 +10,7 @@ for vcfline in vcffile:
 	#Check if the line is where the variant table starts
 	if "#CHROM" in vcfline:
 		#Get the headers for the columns by splitting the vcfline where where finds #CHROM, which indicates the column headers
+		#The last column name will contain a new line if this is not removed
 		vcfline = vcfline.replace("\n","")
 		columnheaders = vcfline.split("\t")
 		
@@ -18,6 +19,14 @@ for vcfline in vcffile:
 	
 	#Get variant table
 	if variantslist == True:
-		print(vcfline)
+		#Convert row to list for analysis
+		#The last column name will contain a new line if this is not removed
+		vcfline = vcfline.replace("\n","")
+		vcfrow = vcfline.split("\t")
+		
+		#Generate HGVS variant description
+		hgvs = "g." + vcfrow[0] + ":" + vcfrow[1] + vcfrow[3] + ">" + vcfrow[4]
+		
+		print(hgvs)
 
 vcffile.close()

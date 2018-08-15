78a56a0afa2c906759debae13d655969114b7054
Richard Crooks <email@rocrooks.co.uk>
2018-08-15 15:05:44 +0100

    Retrieve column headings as a list

--- vcfparser.py
+++ vcfparser.py
@@ -2,8 +2,22 @@
 vcffilename = "TestVCF_Filtered_Annotated.vcf"
 vcffile = open(vcffilename, "r")
 
+#Flag to start parsing the data in the VCF
+variantslist = False
+
 #Read each line in the VCF and print to confirm that VCF can be read
 for vcfline in vcffile:
-	print(vcfline)
+	#Check if the line is where the variant table starts
+	if "#CHROM" in vcfline:
+		#Get the headers for the columns by splitting the vcfline where where finds #CHROM, which indicates the column headers
+		vcfline = vcfline.replace("\n","")
+		columnheaders = vcfline.split("\t")
+		
+		#Flag to tell the script that the following lines are part of the variant table is set
+		variantslist = True
+	
+	#Get variant table
+	if variantslist == True:
+		print(vcfline)
 
 vcffile.close()
